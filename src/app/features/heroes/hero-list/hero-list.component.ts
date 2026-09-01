6d78eabf74b0f64f032c631a8306bf7354ee8ed3
Luis Orlando <luis.orlando.batista@gmail.com>
2026-09-01 19:20:31 +0200

    - Spinner para el cargando de los heroes modo mock

--- src/app/features/heroes/hero-list/hero-list.component.ts
+++ src/app/features/heroes/hero-list/hero-list.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { HeroService } from '../../../core/services/hero.service';
 import { Hero } from '../../../models/hero.model';
 
 @Component({
   selector: 'app-hero-list',
-  imports: [MatTableModule, MatButtonModule, MatIconModule],
+  imports: [MatTableModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './hero-list.component.html',
   styleUrl: './hero-list.component.scss',
 })
@@ -16,6 +17,7 @@ export class HeroListComponent implements OnInit {
   private readonly heroService = inject(HeroService);
 
   readonly heroes = signal<Hero[]>([]);
+  readonly isLoading = signal(true);
   readonly displayedColumns = ['name', 'heroName', 'power', 'universe', 'actions'];
 
   ngOnInit(): void {
@@ -23,6 +25,10 @@ export class HeroListComponent implements OnInit {
   }
 
   private loadHeroes(): void {
-    this.heroService.getAll().subscribe(heroes => this.heroes.set(heroes));
+    this.isLoading.set(true);
+    this.heroService.getAll().subscribe(heroes => {
+      this.heroes.set(heroes);
+      this.isLoading.set(false);
+    });
   }
 }
